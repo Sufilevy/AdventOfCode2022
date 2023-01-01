60258a118da25adbede9e80ac02d0648b4536c7d
Sufilevy <sufilevy@gmail.com>
2023-01-01 11:07:07 +0200

    Finished day 1

---
+++ Day1/Program.fs
@@ -0,0 +1,21 @@
+﻿let caloriesOf (elf: string) =
+    elf.Split '\n'
+    |> Array.filter ((<>) "")
+    |> Array.map int
+    |> Array.sum
+
+let puzzleOne (input: string) = 
+    (input.Split "\n\n")
+    |> Array.map caloriesOf
+    |> Array.max
+
+let puzzleTwo (input: string) =
+    (input.Split "\n\n")
+    |> Array.map caloriesOf
+    |> Array.sortDescending
+    |> Array.take 3
+    |> Array.sum
+
+let file = System.IO.File.ReadAllText("input.txt")
+puzzleOne file |> printfn "%A"
+puzzleTwo file |> printfn "%A"
